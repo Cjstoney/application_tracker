16a0331717002055db190ca6088c19e50936143d
Camerone Stoney <Cameronestoney@yahoo.com>
2021-02-06 17:13:16 -0800

    chore: updates to comments

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -8,11 +8,11 @@ function Login(): JSX.Element {
       <button onClick={() => setSignUp(true)}>Sign Up</button>
       <button onClick={() => setSignUp(false)}>Login</button>
       <div>
-        {/* this div contains the text inputs and submit button */}
         <label htmlFor="email">email</label>
         <input type="text"></input>
         <label htmlFor="password">password</label>
         <input type="password"></input>
+        {/* toggles confirmation of password based on state value */}
         {!!newUser
           ? [
               <label htmlFor="passwordConfirm">confirm password</label>,
