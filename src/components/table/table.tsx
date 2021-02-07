4df54726b5845a07535619edfd565f10e8ca9b20
Rebecca Bompiani <rebeccabompiani@gmail.com>
2021-02-06 21:02:53 -0800

    created Table Component, linked it into App component

---
+++ src/components/table/table.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Table = () => {
+    const [applications, setApplications] = useState([
+        {
+            id: "1278463",
+            jobTitle: "Frontend Engineer",
+            company: "Figma",
+            applicationDate: new Date('05 October 2011 14:48 UTC').toISOString(),
+            link: "http://www.figma.com",
+            companyResponse: "rejected",
+            followedUp: false,
+            connection: "Cam Stoney",
+            notes: "hiring manager is Dan Clevenger, dan@figma.com"
+        },
+        {
+            id: "8478455",
+            jobTitle: "Mentor",
+            company: "The Collab Lab",
+            applicationDate: new Date('05 October 2018 3:05 UTC').toISOString(),
+            link: "https://the-collab-lab.codes/",
+            companyResponse: null,
+            followedUp: true,
+            connection: null,
+            notes: "job posting places heavy focus on UX"
+        }
+    ])
+
+    return (
+        <table>
+            <tr>
+                <th>Title</th>
+                <th>Company</th>
+                <th>Date</th>
+                <th>Link</th>
+                <th>Response</th>
+                <th>Followed Up</th>
+                <th>Connection</th>
+                <th>Notes</th>
+            </tr>
+            {applications.map(app => {
+                return (
+                    <tr key={app.id}>
+                        <td>{app.jobTitle}</td>
+                    </tr>
+                )
+            })}
+        </table>
+    )
+}
+
+export default Table;
