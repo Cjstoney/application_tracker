0f2431040355d90570953d7bd6ef0e9fe16e6782
Camerone Stoney <Cameronestoney@yahoo.com>
2021-02-09 15:59:54 -0800

    test: one more test

--- src/components/login/loginHelpers.test.ts
+++ src/components/login/loginHelpers.test.ts
@@ -9,37 +9,45 @@ jest.spyOn(window, 'alert').mockImplementation(() => {});
 jest.spyOn(Storage.prototype,'setItem').mockImplementation(()=>{})
 
 describe('the authenticateUser function',()=>{
-
+    const email='email@email.com'
+    const password='Password1!'
+    const confirmationPassword='Password1'
+    let newUserStatus:boolean
+    
     beforeEach(()=>{
         jest.clearAllMocks()
         const user = {
             userSub:'123456789'
         } as any
         Auth.signUp = jest.fn().mockResolvedValue(user);
+        Auth.signIn = jest.fn().mockResolvedValue(user);
     })
 
     it('creates a new user correctly',async ()=>{
-        const email='email@email.com'
-        const password='Password1!'
-        const newUserStatus=true
-
+        newUserStatus = true
         const result = await authenticateUser({email,password,confirmationPassword:password,newUserStatus})
         expect(result).not.toBeUndefined()
         expect(Auth.signUp).toHaveBeenCalledTimes(1)
         expect(localStorage.setItem).toHaveBeenCalledTimes(1)
         expect(localStorage.setItem).toHaveBeenCalledWith("appTrackerUser", result.userSub)
     })
-
-    it('does not allow for a user sto sign up with mismatched creds', async()=>{
-        const email='email@email.com'
-        const password='Password1!'
-        const confirmationPassword='Password1'
-        const newUserStatus=true
-
+    
+    it('does not allow for a user to sign up with mismatched creds', async()=>{
+        newUserStatus = true
         const result = await authenticateUser({email,password,confirmationPassword,newUserStatus})
         expect(result).toBeUndefined()
         expect(window.alert).toHaveBeenCalledTimes(1)
         expect(Auth.signUp).not.toHaveBeenCalledTimes(1)
+    })
+
+    it('logs a user in correctly', async ()=>{
+        newUserStatus= false
+        const result = await authenticateUser({email,password,confirmationPassword:password,newUserStatus})
+        expect(result).not.toBeUndefined()
+        expect(Auth.signIn).toHaveBeenCalledTimes(1)
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+        expect(localStorage.setItem).toHaveBeenCalledWith("appTrackerUser",result.userSub)
+
     })
 
 })
