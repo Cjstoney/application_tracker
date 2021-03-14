ae9d0dc35aa519cc2a660386dd7111420cef4073
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-13 18:20:14 -0800

    WIP: need to trigger detection of logged in

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import Login from "./components/login/login";
 import { AppEnums } from "./models/enums";
+import { useEffect } from "react";
 
 Amplify.configure(awsconfig);
 function App() {
   let isLoggedIn: boolean = !!localStorage.getItem(AppEnums.AppTrackerUser);
+  useEffect(() => {
+    isLoggedIn = !!localStorage.getItem(AppEnums.AppTrackerUser);
+  });
   return (
     <div className="App">
       {!!isLoggedIn ? console.log("please login") : <Login />}
