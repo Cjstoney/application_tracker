f13201a5d0c218958e71238283b718b4aa744c4f
Camerone Stoney <Cameronestoney@yahoo.com>
2021-05-22 14:50:03 -0700

    chore: removal of amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./components/footer/footer";
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
 import Login from "./components/login/login";
 
-Amplify.configure(awsconfig);
+// this needs to set the state to either a user or false. Then conditionally render the correct component
+// const []
 function App() {
   return (
     <div className="App">
